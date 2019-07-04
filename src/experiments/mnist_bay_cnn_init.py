fix bayes by backprop by adding step function

src/experiments/mnist_bay_cnn_init.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 from torch.nn import CrossEntropyLoss
 
 from src.models.bayesian_models import GaussianClassifierMNIST
-from src.trains import train, test, test_bayesian, train_bayesian
+from src.trains import train, test, test_bayesian, train_bayesian, test_random
 from src.utils import set_and_print_random_seed, aggregate_data
 from src.get_data import get_mnist
 
@@ -28,6 +28,11 @@ loss_type = args.loss_type
 mu_prior = float(args.mu_prior)
 std_prior = float(args.std_prior)
 
+if mu_prior is None:
+    mu_prior = 0
+
+if std_prior is None:
+    std_prior = 1
 
 mus_prior = (mu_prior, mu_prior)
 stds_prior = (std_prior, std_prior)
@@ -46,30 +51,29 @@ bay_net.to(device)
 criterion = CrossEntropyLoss()
 adam_proba = optim.Adam(bay_net.parameters())
 
-losses, loss_llhs, loss_vps, loss_prs, accs = train_bayesian(bay_net, adam_proba, criterion,
+losses, loss_llhs, loss_vps, loss_prs, accs, max_acc, epoch_max_acc, i_max_acc = train_bayesian(bay_net,
+                                                             adam_proba, criterion,
                                                              epoch, trainloader, loss_type=loss_type,
                                                              output_dir_tensorboard='./output',
+                                                             output_dir_results="./output/weights_training",
                                                              device=device, verbose=True)
 test_acc, test_uncertainty, test_dkls = test_bayesian(bay_net, testloader,
                                                       number_of_tests=number_of_tests, device=device)
-
-
-seed_random = set_and_print_random_seed()
-random_noise = torch.randn(16,1,28,28).to(device)
-output_random = torch.Tensor(number_of_tests, 16, 10)
-for test_idx in range(number_of_tests):
-    output_random[test_idx] = bay_net(random_noise).detach()
-_, random_uncertainty, random_dkl = aggregate_data(output_random)
+random_uncertainty, random_dkl, seed_random = test_random(bay_net, batch_size, 1, 28, number_of_tests,
+                                                          number_of_classes=10, device=device)
 
 res = dict({
     "number of epochs": epoch,
+    "batch_size": batch_size,
     "number of tests": number_of_tests,
     "seed_model": seed_model,
-    "mu_prior": 0,
-    "stds_prior": 1,
+    "mu_prior": mu_prior,
+    "stds_prior": std_prior,
     "rho": rho,
     "sigma initial": log(1 + exp(rho)),
     "train accuracy": accs,
+    "train max acc": max_acc,
+    "train max acc epoch": epoch_max_acc,
     "train loss": losses,
     "train loss llh": loss_llhs,
     "train loss vp": loss_vps,
@@ -83,4 +87,4 @@ res = dict({
 })
 
 torch.save(res, "./output/results.pt")
-torch.save(bay_net.state_dict(), "./output/weights.pt")
+torch.save(bay_net.state_dict(), "./output/final_weights.pt")

src/mnist_example.py
@@ -1,5 +1,4 @@
 #%% Imports
-import os
 
 import matplotlib.pyplot as plt
 from importlib import reload
@@ -7,8 +6,6 @@ import numpy as np
 
 import torch
 import torch.nn as nn
-import torchvision
-import torchvision.transforms as transforms
 import torch.optim as optim
 
 import src.utils as u
@@ -42,33 +39,88 @@ trainloader, testloader = dataset.get_cifar10()
 reload(u)
 reload(bm)
 reload(t)
-trainloader, testloader = dataset.get_mnist(batch_size=16)
-bay_net = bm.GaussianClassifierMNIST(-2, (0, 0), (1, 1), number_of_classes=10)
-bay_net.to(device)
+trainloader, testloader = dataset.get_mnist(batch_size=128)
+det_net = bm.GaussianClassifierMNIST("determinist", (0, 0), (1, 1), number_of_classes=10)
+det_net.to(device)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(bay_net.parameters())
-t.train(bay_net, optimizer, criterion, 5, trainloader=trainloader, device=device, verbose=True)
+optimizer = optim.Adam(det_net.parameters())
+t.train(det_net, optimizer, criterion, 3, output_dir_results='sandbox_results/det',
+        trainloader=trainloader, device=device, verbose=True)
 
- #%%
+#%%
 
 reload(u)
 reload(bm)
 reload(t)
-trainloader, testloader = dataset.get_mnist(batch_size=16)
+trainloader, testloader = dataset.get_mnist(batch_size=128)
 bay_net = bm.GaussianClassifierMNIST(-5, (0, 0), (1, 1), number_of_classes=10)
 bay_net.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(bay_net.parameters())
-t.train_bayesian(bay_net, optimizer, criterion, 2, trainloader, output_dir_tensorboard="./output",
+t.train_bayesian(bay_net, optimizer, criterion, 3, trainloader,
+                 loss_type="bbb",
+                 output_dir_results='sandbox_results/bbb_stepped',
+                 output_dir_tensorboard="./output",
                  device=device, verbose=True);
+
+#%%
+reload(u)
+weights_paths = u.get_file_path_in_dir('sandbox_results/bbb_stepped')
+weights_norms = []
+for path in weights_paths:
+    model = bm.GaussianClassifierMNIST(rho=1)
+    model.load_state_dict(torch.load(path))
+    weights_norms.append(u.compute_weights_norm(model))
+
+plt.plot(weights_norms)
+plt.title("bbb_stepped")
+plt.show()
+#%%
+reload(u)
+weights_paths = u.get_file_path_in_dir('sandbox_results/ce')
+weights_norms = []
+for path in weights_paths:
+    model = bm.GaussianClassifierMNIST()
+    model.load_state_dict(torch.load(path))
+    weights_norms.append(u.compute_weights_norm(model))
+
+plt.plot(weights_norms)
+plt.title("cross entropy")
+plt.show()
+#%%
+weights_paths = u.get_file_path_in_dir('sandbox_results/det')
+weights_norms = []
+for path in weights_paths:
+    model = bm.GaussianClassifierMNIST(rho="determinist")
+    model.load_state_dict(torch.load(path))
+    weights_norms.append(u.compute_weights_norm(model))
+
+plt.plot(weights_norms)
+plt.title("determinist")
+plt.show()
+
+# %%
+t.test_bayesian(bay_net, testloader, 15, device)
+
+#%%
+
+t.test(det_net, testloader, device)
+
+
+#%%
+
 
 
 
 #%%
 
-prev_weights, prev_bias = bay_net.get_previous_weights()
-vp = bay_net.variational_posterior(prev_weights, prev_bias)
-pl = bay_net.prior(prev_weights, prev_bias)
+_, testloader = dataset.get_mnist(batch_size=128)
+get_test_img = iter(testloader)
+img, label = next(get_test_img)
+#%%
+outpt = bay_net(img)
+print("predicted:", np.unique(outpt.argmax(1), return_counts=1))
+print("true:", np.unique(label, return_counts=1))
 
 #%%
 

src/models/bayesian_models.py
@@ -165,7 +165,7 @@ class GaussianLinear(nn.Linear):
 
 class GaussianClassifierMNIST(nn.Module):
 
-    def __init__(self, rho, mus_prior=(0,0), stds_prior=None,
+    def __init__(self, rho=-5, mus_prior=(0,0), stds_prior=None,
                  number_of_classes=10, dim_input=28):
         super().__init__()
         if rho == "determinist" :
@@ -181,16 +181,17 @@ class GaussianClassifierMNIST(nn.Module):
         self.dim_input = dim_input
         self.number_of_classes = number_of_classes
 
-        mu_prior, mu_bias_prior = mus_prior
-        self.mu_prior_init = mu_prior
-        self.mu_bias_prior_init = mu_bias_prior
-        if stds_prior is not None:
-            std_prior, std_bias_prior = stds_prior
-        else:
-            std_prior = math.log(math.exp(rho)+1)
-            std_bias_prior = math.log(math.exp(rho)+1)
-        self.std_prior_init = std_prior
-        self.std_bias_prior_init = std_bias_prior
+        if not self.determinist:
+            mu_prior, mu_bias_prior = mus_prior
+            self.mu_prior_init = mu_prior
+            self.mu_bias_prior_init = mu_bias_prior
+            if stds_prior is not None:
+                std_prior, std_bias_prior = stds_prior
+            else:
+                std_prior = math.log(math.exp(rho)+1)
+                std_bias_prior = math.log(math.exp(rho)+1)
+            self.std_prior_init = std_prior
+            self.std_bias_prior_init = std_bias_prior
 
         self.gaussian_conv1 = GaussianCNN(rho, 1, 16, 3, padding=1)
         self.bn1 = nn.BatchNorm2d(16)
@@ -282,7 +283,7 @@ class GaussianClassifierMNIST(nn.Module):
         std_bias = torch.log(1+torch.exp(rho_bias))
 
         return -1/2 * ((torch.sum((weights - mu)**2 / std) + torch.sum((bias - mu_bias)**2 / std_bias)) +
-                       torch.sum(torch.log(std)) + torch.sum(torch.log(std_bias)))
+                       2*torch.sum(torch.log(std)) + 2*torch.sum(torch.log(std_bias)))
 
     def prior(self, weights, bias):
         '''
@@ -295,7 +296,7 @@ class GaussianClassifierMNIST(nn.Module):
             prior_loss (torch.Tensor): size (1)
         '''
         return -1/2 * ((torch.sum((weights - self.mu_prior)**2 / self.std_prior) +
-                        torch.sum((bias - self.mu_bias_prior)**2 / self.std_bias_prior)))
+                  torch.sum((bias - self.mu_bias_prior)**2 / self.std_bias_prior)))
 
     def get_previous_weights(self, output='vector'):
 
@@ -346,7 +347,7 @@ class GaussianClassifierMNIST(nn.Module):
         self.init_prior()
 
 
-class GaussianClassifierMNIST(nn.Module):
+class GaussianClassifierMNIST_NoBatchNorm(nn.Module):
 
     def __init__(self, rho, mus_prior=(0,0), stds_prior=None,
                  number_of_classes=10, dim_input=28):
@@ -376,10 +377,8 @@ class GaussianClassifierMNIST(nn.Module):
         self.std_bias_prior_init = std_bias_prior
 
         self.gaussian_conv1 = GaussianCNN(rho, 1, 16, 3, padding=1)
-        self.bn1 = nn.BatchNorm2d(16)
         self.pool1 = nn.MaxPool2d(2, 2)
         self.gaussian_conv2 = GaussianCNN(rho, 16, 32, 3, padding=1)
-        self.bn2 = nn.BatchNorm2d(32)
         self.pool2 = nn.MaxPool2d(2, 2)
         self.gaussian_linear = GaussianLinear(rho, 32 * self.dim_input//4*self.dim_input//4, number_of_classes)
 
@@ -415,9 +414,9 @@ class GaussianClassifierMNIST(nn.Module):
             do_determinist = determinist
         else:
             do_determinist = self.determinist
-        output = self.bn1(self.gaussian_conv1(x, determinist=do_determinist))
+        output = self.gaussian_conv1(x, determinist=do_determinist)
         output = self.pool1(F.relu(output))
-        output = self.bn2(self.gaussian_conv2(output, determinist=do_determinist))
+        output = self.gaussian_conv2(output, determinist=do_determinist)
         output = self.pool2(F.relu(output))
         output = output.view(-1, 32*self.dim_input//4*self.dim_input//4)
 

src/trains.py
@@ -3,22 +3,27 @@ import os
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-from src.utils import aggregate_data
+from src.utils import aggregate_data, set_and_print_random_seed
 
 
 def train(model, optimizer, criterion, number_of_epochs, trainloader,
-          output_dir_tensorboard=None, device="cpu", verbose = False):
+          output_dir_tensorboard=None, output_dir_results='sandbox_results', device="cpu", verbose = False):
     return train_bayesian(model, optimizer, criterion, number_of_epochs, trainloader,
                  loss_type='criterion',output_dir_tensorboard=output_dir_tensorboard,
-                 device=device, verbose=verbose)
+                 output_dir_results= output_dir_results, device=device, verbose=verbose)
 
 
 def test(model, testloader, device):
     return test_bayesian(model, testloader, number_of_tests=1, device=device)
 
 
+def uniform(batch_index,number_of_batchs):
+    return 1/number_of_batchs
+
+
 def train_bayesian(model, optimizer, criterion, number_of_epochs, trainloader, loss_type='bbb',
-                   output_dir_tensorboard=None, device="cpu", verbose=False):
+                   step_function=uniform,
+                   output_dir_tensorboard=None, output_dir_results=None, device="cpu", verbose=False):
     '''
     Train the model in a bayesian fashion, meaning the loss is different.
     Args:
@@ -27,7 +32,8 @@ def train_bayesian(model, optimizer, criterion, number_of_epochs, trainloader, l
         criterion (function): how do we compute the likelihood
         number_of_epochs (int): how long do we train our model
         trainloader (torch.utils.data.dataloader.DataLoader): train data
-        loss (str): which type of loss. Chose "bbb" (Bayes By Backprop) or "criterion" (CrossEntropy)
+        loss_type (str): which type of loss. Chose "bbb" (Bayes By Backprop) or "criterion" (CrossEntropy)
+        step_function (function): takes as args (number of batchs, length of batch) and returns the weight to give to KL
         output_dir_tensorboard (str): output directory in which to save the tensorboard
         device (torch.device || str): cpu or gpu
         verbose (Bool): print training steps or not
@@ -38,6 +44,9 @@ def train_bayesian(model, optimizer, criterion, number_of_epochs, trainloader, l
         loss_vps (list): list of the variational posterior loss for each epoch (q(W|D))
         loss_prs (list): list of the prior loss for each epoch (P(W))
         train_accs (list): list of the accuracies for each epoch
+        max_acc (float): the maximum accuracy obtained by the net
+        epoch_max_acc (int): the epoch where the max acc is obtained
+        i_max_acc (int): the batch_idx where the epoch is obtained
 
     '''
 
@@ -49,6 +58,12 @@ def train_bayesian(model, optimizer, criterion, number_of_epochs, trainloader, l
             writer_loss_pr = SummaryWriter(log_dir=os.path.join(output_dir_tensorboard, "loss_pr"))
         writer_accs = SummaryWriter(log_dir=os.path.join(output_dir_tensorboard, "accuracy"))
         tensorboard_idx = 0
+    if output_dir_results is not None:
+        weights_writer_idx = 0
+        if not os.path.exists(output_dir_results):
+            os.mkdir(output_dir_results)
+
+    max_acc = 0
 
     model.train()
     loss_accs = [list() for _ in range(number_of_epochs)]
@@ -70,6 +85,9 @@ def train_bayesian(model, optimizer, criterion, number_of_epochs, trainloader, l
         number_of_labels = 0
 
         for i, data in enumerate(trainloader, 0):
+            M = len(trainloader)
+            kl_weight = step_function(i, M)
+
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = [x.to(device) for x in data]
 
@@ -83,7 +101,7 @@ def train_bayesian(model, optimizer, criterion, number_of_epochs, trainloader, l
                 weights_used, bias_used = model.get_previous_weights()
                 loss_varational_posterior = model.variational_posterior(weights_used, bias_used)
                 loss_prior = -model.prior(weights_used, bias_used)
-                loss = loss_varational_posterior + loss_prior + loss_likelihood
+                loss = kl_weight*(loss_varational_posterior + loss_prior) + loss_likelihood
             elif loss_type == 'criterion':
                 loss = loss_likelihood
             else:
@@ -109,6 +127,10 @@ def train_bayesian(model, optimizer, criterion, number_of_epochs, trainloader, l
                     current_loss_vp = running_loss_vp / number_of_data
                     current_loss_pr = running_loss_pr / number_of_data
                 current_acc = number_of_correct_labels / number_of_labels
+                if max_acc < current_acc:
+                    max_acc = current_acc
+                    epoch_max_acc = epoch
+                    i_max_acc = i
                 if verbose:
                     if loss_type == 'bbb':
                         print(f'Train: [{epoch + 1}, {i + 1}/{number_of_data}] '
@@ -138,6 +160,11 @@ def train_bayesian(model, optimizer, criterion, number_of_epochs, trainloader, l
                     writer_accs.add_scalar('Train accuracy', current_acc, tensorboard_idx)
                     tensorboard_idx += 1
 
+                if output_dir_results is not None:
+                    file_idx = 'weight' + str(weights_writer_idx) + '.pt'
+                    torch.save(model.state_dict(), os.path.join(output_dir_results, file_idx))
+                    weights_writer_idx += 1
+
                 running_loss = 0.0
                 if loss_type == 'bbb':
                     running_loss_llh = 0.0
@@ -154,9 +181,9 @@ def train_bayesian(model, optimizer, criterion, number_of_epochs, trainloader, l
             writer.close()
     print('Finished Training')
     if loss_type == 'bbb':
-        return loss_accs, loss_llhs, loss_vps, loss_prs, train_accs
+        return loss_accs, loss_llhs, loss_vps, loss_prs, train_accs, max_acc, epoch_max_acc, i_max_acc
     else:
-        return loss_accs, 0, 0, 0, train_accs
+        return loss_accs, [[0]], [[0]], [[0]], train_accs, max_acc, epoch_max_acc, i_max_acc
 
 
 def test_bayesian(model, testloader, number_of_tests, device):
@@ -182,3 +209,13 @@ def test_bayesian(model, testloader, number_of_tests, device):
     accuracy = (all_correct_labels / number_of_samples).item()
 
     return accuracy, all_uncertainties, all_dkls
+
+
+def test_random(model, batch_size, img_channels, img_dim, number_of_tests, number_of_classes, random_seed=None, device='cpu'):
+    seed = set_and_print_random_seed(random_seed)
+    random_noise = torch.randn(batch_size, img_channels, img_dim, img_dim).to(device)
+    output_random = torch.Tensor(number_of_tests, batch_size, number_of_classes)
+    for test_idx in range(number_of_tests):
+        output_random[test_idx] = model(random_noise).detach()
+    _, random_uncertainty, random_dkl = aggregate_data(output_random)
+    return random_uncertainty, random_dkl, seed