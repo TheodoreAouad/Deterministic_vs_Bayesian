be21652da9fdd2b217d891f64c5ba1286ded9701
theodore <theodoreaouad@hotmail.fr>
2019-08-14 10:22:46 +0200

    improve correlation figures

---
+++ experiments/uncertainty_unseen_dataset_det.py
@@ -0,0 +1,162 @@
+import argparse
+from math import log, exp
+
+import pandas as pd
+import torch
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.nn import CrossEntropyLoss
+
+from src.loggers.losses.base_loss import BaseLoss
+from src.loggers.losses.bbb_loss import BBBLoss
+from src.loggers.observables import AccuracyAndUncertainty
+from src.models.bayesian_models.gaussian_classifiers import GaussianClassifier
+from src.tasks.trains import train_bayesian_modular, uniform
+from src.tasks.evals import eval_bayesian
+from src.uncertainty_measures import get_all_uncertainty_measures
+from src.utils import set_and_print_random_seed, save_to_file
+from src.dataset_manager.get_data import get_mnist, get_omniglot, get_cifar10
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset", help="which dataset to test the model", choices=["cifar10", "omniglot"], type=str,
+                    default="omniglot")
+parser.add_argument("--rho", help="variable symbolizing the variance. std = log(1+exp(rho))",
+                    type=float, default=-5)
+parser.add_argument("--epoch", help="number of times we train the model on the same data",
+                    type=int, default=3)
+parser.add_argument("--batch_size", help="number of batches to split the data into",
+                    type=int, default=32)
+parser.add_argument("--number_of_tests", help="number of evaluations to perform for each each image to check for "
+                                              "uncertainty", type=int, default=10)
+parser.add_argument("--loss_type", help="which loss to use", choices=["bbb", "criterion"], type=str,
+                    default="bbb")
+parser.add_argument("--std_prior", help="the standard deviation of the prior", type=float, default=1)
+args = parser.parse_args()
+
+save_to_file(vars(args), './output/arguments.pkl')
+
+dataset = args.dataset.lower()
+rho = args.rho
+epoch = args.epoch
+batch_size = args.batch_size
+number_of_tests = args.number_of_tests
+loss_type = args.loss_type
+std_prior = args.std_prior
+
+stds_prior = (std_prior, std_prior)
+
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+device = torch.device(device)
+
+transform = transforms.Compose([
+        transforms.Resize(28),
+        transforms.ToTensor()
+    ])
+if dataset == "omniglot":
+    unseen_loader = get_omniglot(transform=transform, batch_size=batch_size, download=False)
+elif dataset == "cifar10":
+    transform = transforms.Compose([
+        transforms.Grayscale(),
+        transform
+    ])
+    _, unseen_loader = get_cifar10(transform=transform, batch_size=batch_size)
+else:
+    raise TypeError('Dataset not yet implemented')
+
+trainloader, valloader, evalloader = get_mnist(batch_size=batch_size)
+
+
+seed_model = set_and_print_random_seed()
+bay_net = GaussianClassifier(rho=rho, stds_prior=stds_prior, dim_input=28, number_of_classes=10)
+bay_net.to(device)
+criterion = CrossEntropyLoss()
+if loss_type == 'bbb':
+    step_function = uniform
+    loss = BBBLoss(bay_net, criterion, step_function)
+else:
+    loss = BaseLoss(criterion)
+
+optimizer = optim.Adam(bay_net.parameters())
+observables = AccuracyAndUncertainty()
+train_bayesian_modular(
+    bay_net,
+    optimizer,
+    loss,
+    observables,
+    number_of_tests=number_of_tests,
+    number_of_epochs=epoch,
+    trainloader=trainloader,
+    valloader=valloader,
+    output_dir_tensorboard='./output',
+    device=device,
+    verbose=True,
+)
+
+print("Evaluation on MNIST ...")
+seen_eval_acc, all_outputs_eval_seen = eval_bayesian(
+    bay_net,
+    evalloader,
+    number_of_tests=number_of_tests,
+    device=device
+)
+seen_eval_vrs, seen_eval_pes, seen_eval_mis = get_all_uncertainty_measures(all_outputs_eval_seen)
+print("Finished evaluation on MNIST.")
+
+print(f"Evavuation on {dataset} ...")
+_, all_outputs_eval_unseen = eval_bayesian(
+    bay_net,
+    unseen_loader,
+    number_of_tests=number_of_tests,
+    device=device
+)
+unseen_eval_vrs, unseen_eval_pes, unseen_eval_mis = get_all_uncertainty_measures(all_outputs_eval_unseen)
+print("Finished evaluation on ", dataset)
+
+print(f"MNIST: {round(100*seen_eval_acc,2)} %, "
+      f"Variation-ratios:{seen_eval_vrs.mean()}, "
+      f"Predictive Entropy:{seen_eval_pes.mean()}, "
+      f"Mutual information:{seen_eval_mis.mean()}")
+print(f"{dataset}: Variation-ratios:{unseen_eval_vrs.mean()}, "
+      f"Predictive Entropy:{unseen_eval_pes.mean()}, ",
+      f"Mutual Information:{unseen_eval_mis.mean()}")
+res = pd.DataFrame.from_dict({
+    'loss_type': loss_type,
+    'dataset': dataset,
+    'number of epochs': epoch,
+    'batch_size': batch_size,
+    'number of tests': number_of_tests,
+    'seed_model': seed_model,
+    'stds_prior': std_prior,
+    'rho': rho,
+    'sigma initial': log(1 + exp(rho)),
+    'train accuracy': observables.logs['train_accuracy_on_epoch'],
+    'train max acc': observables.max_train_accuracy_on_epoch,
+    'train max acc epoch': observables.epoch_with_max_train_accuracy,
+    'train loss': loss.logs.get('total_loss', -1),
+    'train loss llh': loss.logs.get('likelihood', -1),
+    'train loss vp': loss.logs.get('variational_posterior', -1),
+    'train loss pr': loss.logs.get('prior', -1),
+    'val accuracy': observables.logs['val_accuracy'],
+    'val vr': observables.logs['val_uncertainty_vr'],
+    'val predictive entropy': observables.logs['val_uncertainty_pe'],
+    'val mi': observables.logs['val_uncertainty_mi'],
+    "eval accuracy": seen_eval_acc,
+    "seen uncertainty vrs": seen_eval_vrs,
+    "seen uncertainty pes": seen_eval_pes,
+    "seen uncertainty mis": seen_eval_mis,
+    "unseen uncertainty vrs": unseen_eval_vrs,
+    "unseen uncertainty pes": unseen_eval_pes,
+    "unseen uncertainty mis": unseen_eval_mis
+})
+
+
+save_to_file(loss, './output/loss.pkl')
+save_to_file(observables, './output/TrainingLogs.pkl')
+torch.save(all_outputs_eval_unseen, './output/softmax_outputs_eval_unseen.pt')
+torch.save(all_outputs_eval_seen, './output/softmax_outputs_eval_seen.pt')
+# torch.save(res, "./output/results.pt")
+res.to_pickle('./output/results.pkl')
+torch.save(bay_net.state_dict(), "./output/final_weights.pt")
--- scripts/get_correlation_figures_det.py
+++ scripts/get_correlation_figures_det.py
@@ -8,12 +8,12 @@ import numpy as np
 from src.dataset_manager.get_data import get_mnist
 from src.models.bayesian_models.gaussian_classifiers import GaussianClassifier
 from src.tasks.evals import eval_bayesian, eval_random
-from src.uncertainty_measures import get_predictions_from_multiple_tests, get_all_uncertainty_measures
+from src.uncertainty_measures import get_predictions_from_multiple_tests, get_all_uncertainty_measures, \
+    get_all_uncertainty_measures_not_bayesian
 from src.utils import load_from_file, get_file_and_dir_path_in_dir
 
 ###### TO CHANGE ###########
-group_nb = '172'
-idx_in_group = 0
+group_nb = '180'
 nb_of_batches = 1000
 size_of_batch = 100
 nb_of_random = 5000
@@ -37,12 +37,12 @@ def get_exact_batch_size(size_of_batch, total_nb_sample):
 
 exp_path = pathlib.Path('polyaxon_results/groups')
 _, all_dirs = get_file_and_dir_path_in_dir(exp_path / group_nb, 'argumen')
-dirpath = all_dirs[idx_in_group]
+dirpath = all_dirs[0]
 exp_nb = dirpath.split('/')[-1]
 dirpath = pathlib.Path(dirpath)
 
 arguments = load_from_file(dirpath / 'arguments.pkl')
-save_path = pathlib.Path(f'results/correlations_figures/{arguments["loss_type"]}')
+save_path = pathlib.Path(f'results/correlations_figures/determinist')
 save_path.mkdir(parents=True, exist_ok=True)
 final_weigths = torch.load(dirpath / 'final_weights.pt', map_location='cpu')
 
@@ -50,13 +50,12 @@ _, _, evalloader = get_mnist(split_val=0, batch_size=128, shuffle=False)
 true_labels_mnist = evalloader.dataset.targets.float()
 nb_of_mnist = len(evalloader.dataset)
 
-bay_net_trained = GaussianClassifier(
-    rho=arguments['rho'],
-    stds_prior=(arguments['std_prior'], arguments['std_prior']),
+det_net_trained = GaussianClassifier(
+    rho='determinist',
     number_of_classes=10,
     dim_input=28,
 )
-bay_net_trained.load_state_dict(final_weigths)
+det_net_trained.load_state_dict(final_weigths)
 
 # Get outputs of evaluation on MNIST and on Random
 if do_eval_mnist:
@@ -64,28 +63,23 @@ if do_eval_mnist:
     evalloader.dataset.data = evalloader.dataset.data[shuffle_eval]
     evalloader.dataset.targets = evalloader.dataset.targets[shuffle_eval]
     true_labels_mnist = evalloader.dataset.targets.float()
-    print('Evaluation on MNIST ...')
-    eval_acc, all_eval_outputs = eval_bayesian(bay_net_trained, evalloader,
-                                               number_of_tests=arguments['number_of_tests'])
+    print('Evaluation on MNIST .s..')
+    eval_acc, all_eval_outputs = eval_bayesian(det_net_trained, evalloader, number_of_tests=1)
     print('Finished evaluation on MNIST.')
 output_random = torch.Tensor()
 if nb_of_random > 0:
     print('Evaluation on random ...')
     output_random, _ = eval_random(
-        bay_net_trained,
+        det_net_trained,
         batch_size=nb_of_random,
         img_channels=1,
         img_dim=28,
-        number_of_tests=arguments['number_of_tests'],
+        number_of_tests=1,
         show_progress=True,
     )
     print('Finished evaluation on random.')
-# Get the labels of the evaluation on MNIST, and put -1 for the labels of Random
 all_outputs = torch.cat((all_eval_outputs, output_random), 1)
 total_nb_of_data = len(true_labels_mnist) + nb_of_random
-predicted_labels = get_predictions_from_multiple_tests(all_outputs).float()
-true_labels = torch.cat((true_labels_mnist, -1 + torch.zeros(nb_of_random)))
-correct_labels = (predicted_labels == true_labels)
 
 # Shuffle data between rand and mnist
 all_outputs_shuffled = torch.Tensor()
@@ -99,6 +93,10 @@ for i in range(nb_of_batches):
     batch_label = torch.cat((true_labels_mnist[idx_m[:size_of_batch - k]], -1 + torch.zeros(k)))
     true_labels_shuffled = torch.cat((true_labels_shuffled, batch_label))
 
+# Get the labels of the evaluation on MNIST, and put -1 for the labels of Random
+predicted_labels = get_predictions_from_multiple_tests(all_outputs).float()
+true_labels = torch.cat((true_labels_mnist, -1 + torch.zeros(nb_of_random)))
+correct_labels = (predicted_labels == true_labels)
 # Shuffled data, get predictions
 predicted_labels_shuffled = get_predictions_from_multiple_tests(all_outputs_shuffled).float()
 correct_labels_shuffled = (predicted_labels_shuffled == true_labels_shuffled)
@@ -115,14 +113,12 @@ prop_of_rand = random_idxs_reshaped.mean(0)
 labels_not_shuffled = true_labels.reshape(total_nb_of_data // real_size_of_batch, real_size_of_batch).mean(1) == -1
 
 # get uncertainties
-vr_shuffled, pe_shuffled, mi_shuffled = get_all_uncertainty_measures(all_outputs_shuffled)
-vr, pe, mi = get_all_uncertainty_measures(all_outputs)
-vr_regrouped_shuffled = vr_shuffled.reshape(size_of_batch, nb_of_batches).mean(0)
+unc_soft_shuffled, pe_shuffled = get_all_uncertainty_measures_not_bayesian(all_outputs_shuffled)
+unc_soft, pe = get_all_uncertainty_measures_not_bayesian(all_outputs)
+unc_soft_regrouped_shuffled = unc_soft_shuffled.reshape(size_of_batch, nb_of_batches).mean(0)
 pe_regrouped_shuffled = pe_shuffled.reshape(size_of_batch, nb_of_batches).mean(0)
-mi_regrouped_shuffled = mi_shuffled.reshape(size_of_batch, nb_of_batches).mean(0)
-vr_regrouped = vr.reshape(total_nb_of_data // real_size_of_batch, real_size_of_batch).mean(1)
+unc_soft_regrouped = unc_soft.reshape(total_nb_of_data // real_size_of_batch, real_size_of_batch).mean(1)
 pe_regrouped = pe.reshape(total_nb_of_data // real_size_of_batch, real_size_of_batch).mean(1)
-mi_regrouped = mi.reshape(total_nb_of_data // real_size_of_batch, real_size_of_batch).mean(1)
 
 
 # plot graphs
@@ -130,34 +126,24 @@ try: arguments['nb_of_tests'] = arguments.pop('number_of_tests')
 except KeyError: pass
 plt.figure(figsize=(8, 10))
 plt.suptitle(arguments, wrap=True)
-plt.subplot(321)
-plt.scatter(vr_regrouped, accuracies, c=labels_not_shuffled)
+plt.subplot(221)
+plt.scatter(unc_soft_regrouped, accuracies, c=labels_not_shuffled)
 plt.ylabel('accuracy')
-plt.title('VR - not shuffled')
-plt.subplot(322)
-plt.scatter(vr_regrouped_shuffled, accuracies_shuffled, c=prop_of_rand)
+plt.title('Uncertainty Softmax - not shuffled')
+plt.subplot(222)
+plt.scatter(unc_soft_regrouped_shuffled, accuracies_shuffled, c=prop_of_rand)
 plt.ylabel('accuracy')
-plt.title('VR - shuffled')
+plt.title('Uncertainty Softmax - shuffled')
 cbar = plt.colorbar()
 cbar.set_label('random ratio', rotation=270)
-plt.subplot(323)
+plt.subplot(223)
 plt.scatter(pe_regrouped, accuracies, c=labels_not_shuffled)
 plt.ylabel('accuracy')
 plt.title('PE - not shuffled')
-plt.subplot(324)
+plt.subplot(224)
 plt.scatter(pe_regrouped_shuffled, accuracies_shuffled, c=prop_of_rand)
 plt.ylabel('accuracy')
 plt.title('PE - shuffled')
-cbar = plt.colorbar()
-cbar.set_label('random ratio', rotation=270)
-plt.subplot(325)
-plt.title('MI - not shuffled')
-plt.scatter(mi_regrouped, accuracies, c=labels_not_shuffled)
-plt.ylabel('accuracy')
-plt.subplot(326)
-plt.title('MI - shuffled')
-plt.scatter(mi_regrouped_shuffled, accuracies_shuffled, c=prop_of_rand)
-plt.ylabel('accuracy')
 cbar = plt.colorbar()
 cbar.set_label('random ratio', rotation=270)
 
