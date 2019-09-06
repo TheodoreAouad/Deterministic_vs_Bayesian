5a4be097236dd54fc9b316b37cd1dc2cf242e69a
theodore <theodoreaouad@hotmail.fr>
2019-09-06 15:27:25 +0200

    fix basic_selective_classifier.py to run faster

--- Makefile
+++ Makefile
@@ -8,13 +8,13 @@ pull-deadzone:
 	rsync -ave ssh muaddib:/home/muaddib/Sicara/theodore/BayesianFewShotExperiments/results/deadzones ./results/
 
 write-all-results-csv:
-	python scripts/write_all_results_csv.py --polyaxon_results_path polyaxon_results --polyaxon_type groups --group_nb $(GROUP_NB)
+	python scripts/write_csv/write_all_results_csv.py --polyaxon_results_path polyaxon_results --polyaxon_type groups --group_nb $(GROUP_NB)
 
 write-specific-results-csv-random-test:
-	python scripts/write_only_some_columns_in_csv.py --which_parameters 'makefile_specifications/parameters.txt' --which_values 'makefile_specifications/values_for_random_testset.txt' --exp_nb $(GROUP_NB)
+	python scripts/write_csv/write_only_some_columns_in_csv.py --which_parameters 'makefile_specifications/parameters.txt' --which_values 'makefile_specifications/values_for_random_testset.txt' --exp_nb $(GROUP_NB)
 
 write-specific-results-csv-unseen-test:
-	python scripts/write_only_some_columns_in_csv.py --which_parameters 'makefile_specifications/parameters.txt' --which_values 'makefile_specifications/values_for_unseen_testset.txt' --exp_nb $(GROUP_NB)
+	python scripts/write_csv/write_only_some_columns_in_csv.py --which_parameters 'makefile_specifications/parameters.txt' --which_values 'makefile_specifications/values_for_unseen_testset.txt' --exp_nb $(GROUP_NB)
 
 test:
 	python -m pytest src/
---
+++ scripts/crude_uncertainty_analysis/compute_densities.py
@@ -0,0 +1,93 @@
+"""
+This script is used to create the figures of accuracies against uncertainty. This is used to get results of a single
+experiment. It cannot be used to create a figure across multiple experiments.
+To use it, change the arguments in the TO CHANGE box.
+"""
+import pathlib
+from time import time
+
+import torch
+
+from scripts.utils import compute_figures, compute_density_train_seen_unseen, get_seen_outputs_and_labels, get_unseen_outputs, \
+    get_trained_model_and_args_and_groupnb, get_train_outputs
+
+###### TO CHANGE ###########
+# exp_nbs = ['3713', '3719', '3749', '3778', '3716', '3722', '3752', '3781', '3832', '3834', '3839',
+#            '3840', '3842', '3851', '3861', '3864']
+
+exp_nbs = ['3713', '3832']
+nb_of_batches = 1000
+size_of_batch = 100
+nb_of_random = 5000
+do_compute_correlation = False
+do_compute_histogram = True
+show_fig = True
+save_fig = False
+do_eval_mnist = True
+
+
+############################
+
+
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+device = torch.device(device)
+print(device)
+start_time = time()
+for exp_nb in exp_nbs:
+    print(f'{exp_nb}')
+
+    bay_net_trained, arguments, group_nb = get_trained_model_and_args_and_groupnb(exp_nb)
+
+    all_eval_outputs, true_labels_mnist = get_seen_outputs_and_labels(
+        bay_net_trained,
+        arguments,
+        device=device,
+    )
+
+    all_outputs_unseen = get_unseen_outputs(
+        bay_net_trained,
+        arguments,
+        nb_of_random,
+        device=device,
+    )
+
+    if do_compute_correlation:
+        save_path_cor = pathlib.Path(f'results/correlations_figures/{arguments.get("loss_type", "determinist")}')
+        save_path_cor.mkdir(parents=True, exist_ok=True)
+        save_path_cor = save_path_cor / f'{group_nb}_{exp_nb}_correlation_uncertainty_error.png'
+
+        compute_figures(
+            arguments=arguments,
+            all_outputs_seen=all_eval_outputs,
+            true_labels_seen=true_labels_mnist,
+            all_outputs_unseen=all_outputs_unseen,
+            nb_of_batches=nb_of_batches,
+            size_of_batch=size_of_batch,
+            scale='linear',
+            show_fig=show_fig,
+            save_fig=save_fig,
+            save_path=save_path_cor,
+            figsize=(10, 10),
+        )
+
+    if do_compute_histogram:
+        all_outputs_train = get_train_outputs(
+            bay_net_trained,
+            arguments,
+            device=device,
+        )
+        save_path_hists = pathlib.Path(f'results/uncertainty_density/{arguments.get("loss_type", "determinist")}')
+        save_path_hists.mkdir(parents=True, exist_ok=True)
+        save_path_hists = save_path_hists / f'{group_nb}_{exp_nb}_uncertainty_density.png'
+
+        compute_density_train_seen_unseen(arguments=arguments, all_outputs_train=all_outputs_train,
+                                          all_outputs_seen=all_eval_outputs, all_outputs_unseen=all_outputs_unseen,
+                                          show_fig=show_fig, save_fig=save_fig, save_path=save_path_hists,
+                                          figsize=(12, 10))
+
+    print(f'Time Elapsed:{round(time() - start_time)} s.')
+
+
